3fe9240de945493a8ab5d4627bb83e5a3c63e83e
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 17:31:31 +0900

    route.tsにhandlerを呼び出すようにした

--- src/app/api/webhooks/user/user-deleted/route.ts
+++ src/app/api/webhooks/user/user-deleted/route.ts
@@ -1,50 +1,10 @@
-import { sql } from "@vercel/postgres";
-import { WebhookEvent } from "@clerk/clerk-sdk-node";
-import crypto from 'crypto';
 import { NextApiRequest, NextApiResponse } from "next";
+import handler from "@/app/api/webhooks/user/user-deleted/handler"; 
 
-// Webhook署名シークレット
-const webhookSecret = process.env.WEBHOOK_SECRET_USER_DELETED as string;
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const evt = req.body.evt as WebhookEvent;
-  const receivedSignature = req.headers['x-clerk-webhook-signature'];
-
-  // 署名を計算
-  const expectedSignature = crypto
-    .createHmac('sha256', webhookSecret)
-    .update(JSON.stringify(evt))
-    .digest('base64');
-
-  // 署名の検証
-  if (receivedSignature === expectedSignature) {
-    console.log('Webhook signature is valid.');
-
-    switch (evt.type) {
-    
-      case 'user.deleted':
-        // UserJSON.user_id is a string
-        const user_id = evt.data.id
-  
-        try {
-          await sql`
-            DELETE FROM users WHERE user_id = ${user_id}
-          `;
-          console.log('User deleting successfully.');
-        } catch (error) {
-          console.error('Error deleting user:', error);
-        }
-        break;
-  
-      default:
-        console.log('Unsupported event type:', evt.type);
-    }
-    
-    res.status(200).end('OK');
+export default async function route(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    await handler(req, res);
   } else {
-    console.warn('Webhook signature is not valid. Rejecting request.');
-    res.status(401).end('Unauthorized');
+    res.status(405).end("Method Not Allowed");
   }
-};
-
-export default handler;
+}
---
+++ src/app/api/webhooks/user/user-updated/handler.ts
@@ -0,0 +1,68 @@
+import { sql } from "@vercel/postgres";
+import { WebhookEvent } from "@clerk/clerk-sdk-node";
+import crypto from 'crypto';
+import { NextApiRequest, NextApiResponse } from "next";
+
+// Webhook署名シークレット
+const webhookSecret = process.env.WEBHOOK_SECRET_USER_UPDATED as string;
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const evt = req.body.evt as WebhookEvent;
+  const receivedSignature = req.headers['x-clerk-webhook-signature'];
+
+  // 署名を計算
+  const expectedSignature = crypto
+    .createHmac('sha256', webhookSecret)
+    .update(JSON.stringify(evt))
+    .digest('base64');
+
+  // 署名の検証
+  if (receivedSignature === expectedSignature) {
+    console.log('Webhook signature is valid.');
+
+    switch (evt.type) {
+      case 'user.updated':
+        // UserJSON.id is a string
+        const user_id = evt.data.id;
+        // UserJSON.username is a string
+        const username = evt.data.username;
+        // UserJSON.first_name is a string
+        const firstName = evt.data.first_name;
+        // UserJSON.last_name is a string
+        const lastName = evt.data.last_name;
+        // UserJSON.email_addresses[0].email_address is a string
+        const email_address = evt.data.email_addresses[0].email_address;
+        // UserJSON.image_url is a string
+        const image_url = evt.data.image_url;
+  
+        try {
+          await sql`
+            UPDATE users SET 
+              username = ${username},
+              firstName = ${firstName},
+              lastName = ${lastName},
+              email_address = ${email_address},
+              image_url = ${image_url}
+  
+            WHERE user_id = ${user_id}
+          `;
+          console.log('User updating successfully.');
+          res.status(200).end('OK');
+        } catch (error) {
+          console.error(`Error updating user (user_id: ${user_id}):`, error);
+          res.status(500).end('Internal Server Error');
+        }
+        break;
+  
+      default:
+        console.log('Unsupported event type:', evt.type);
+    }
+
+    res.status(200).end('OK');
+  } else {
+    console.warn('Webhook signature is not valid. Rejecting request.');
+    res.status(401).end('Unauthorized');
+  }
+};
+
+export default handler;
