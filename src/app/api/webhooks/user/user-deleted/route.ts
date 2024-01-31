4dee75c53084c9dfe68d67606d86bda9454fdb6e
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 20:18:10 +0900

    あらかたwebhooksの設定完了

--- src/app/api/webhooks/user/user-deleted/route.ts
+++ src/app/api/webhooks/user/user-deleted/route.ts
@@ -1,10 +1,72 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import handler from "@/app/api/webhooks/user/user-deleted/handler"; 
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { sql } from '@vercel/postgres';
+ 
+export async function POST(req: Request) {
+ 
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET_USER_DELETED;
+ 
+  if (!WEBHOOK_SECRET) {
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
+  }
+ 
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+ 
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    });
+  }
+ 
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+ 
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
+ 
+  let evt: WebhookEvent;
+ 
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400
+    });
+  }
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    await handler(req, res);
-  } else {
-    res.status(405).end("Method Not Allowed");
+  switch (evt.type) {
+    
+    case 'user.deleted':
+      // UserJSON.user_id is a string
+      const user_id = evt.data.id;
+
+      try {
+        await sql`
+          DELETE FROM users WHERE user_id = ${user_id}
+        `;
+        console.log('User deleting successfully.');
+        return new Response('OK', {status: 200});
+      } catch (error) {
+        console.error('Error deleting user:', error);
+        return new Response('Internal Server Error', {status: 500});
+      }
+
+    default:
+      console.log('Unsupported event type:', evt.type);
   }
 }
+ 
