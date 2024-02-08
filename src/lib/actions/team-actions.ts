ced070c7f373971367f86ab7b47b3dcf86fc73ba
BuildTools <unconfigured@null.spigotmc.org>
2024-02-08 15:09:15 +0900

    useRouterをimportするパスの修正

--- src/lib/actions/team-actions.ts
+++ src/lib/actions/team-actions.ts
@@ -3,7 +3,7 @@
 import { TeamSchema } from "@/prisma-types";
 import { PrismaClient } from "@prisma/client"; 
 import { revalidatePath } from "next/cache";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 
 const prisma = new PrismaClient();
 
